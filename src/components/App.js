8d143fcd3d1930438f5dd03cafef9fc8f32cede0
VishalSaini7272 <sainivishal156@gmail.com>
2025-11-02 18:04:31 +0530

    Refactor App component and integrate ToDoList

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,15 @@
 
-// import React from "react";
-// import './../styles/App.css';
 
-// const App = () => {
-//   return (
-//     <div>
-//         {/* Do not remove the main div */}
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-import React, { useState } from "react";
-import Todo from "./Todo";
+import React from "react";
 import './../styles/App.css';
+import ToDoList from "./ToDoList";
 
-function App() {
-  const [task, setTask] = useState("");        // stores current input
-  const [todos, setTodos] = useState([]);      // stores all todo items
-
-  const handleAddTodo = () => {
-    if (task.trim() === "") return; // prevent empty input
-    setTodos([...todos, task]);
-    setTask("");
-  };
-
-  const handleDeleteTodo = (index) => {
-    const updatedTodos = todos.filter((_, i) => i !== index);
-    setTodos(updatedTodos);
-  };
-
+const App = () => {
   return (
-    <div id="main">
-      <h1 className="title">React To-Do List</h1>
-
-      <div className="todo-input">
-        <input
-          id="task-input"
-          type="text"
-          placeholder="Enter a task..."
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <button id="add-btn" onClick={handleAddTodo}>
-          Add Todo
-        </button>
-      </div>
-
-      <Todo todos={todos} handleDeleteTodo={handleDeleteTodo} />
+    <div>
+      <ToDoList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
