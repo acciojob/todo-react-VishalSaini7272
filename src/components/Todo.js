afe5705c8fc7bc3f19705e1bbbf43f80a2067fac
VishalSaini7272 <sainivishal156@gmail.com>
2025-11-02 17:40:43 +0530

    Implement Todo component with delete functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,1 +1,31 @@
 
+
+function Todo({ todos, handleDeleteTodo }) {
+  return (
+    <div className="todo-list">
+      {todos.length === 0 ? (
+        <p className="empty-text">No tasks yet! Add one above 👆</p>
+      ) : (
+        todos.map((todo, index) => (
+          <div
+            key={index}
+            className="todo-item"
+            data-test-id="todo-item"
+          >
+            <span className="task-text">{todo}</span>
+            <button
+              className="delete-btn"
+              onClick={() => handleDeleteTodo(index)}
+              data-test-id={`delete-btn-${index}`}
+            >
+              Delete
+            </button>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Todo;
+
